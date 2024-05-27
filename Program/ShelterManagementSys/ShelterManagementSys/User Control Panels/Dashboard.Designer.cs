0e1a8e0133157f44a159c3f2d182e4e015f64972
Dalma <dalmicsku72@gmail.com>
2024-05-27 23:24:13 +0200

    Dashboard design kialakítása

---
+++ Program/ShelterManagementSys/ShelterManagementSys/User Control Panels/Dashboard.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ShelterManagementSys
+{
+    partial class Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dashboard_AWTBA = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dashboard_AA = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dashboard_TA = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Location = new System.Drawing.Point(18, 184);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(817, 314);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(18, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(817, 145);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.panel5.Controls.Add(this.dashboard_AWTBA);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.panel5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel5.Location = new System.Drawing.Point(551, 18);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(242, 110);
+            this.panel5.TabIndex = 1;
+            // 
+            // dashboard_AWTBA
+            // 
+            this.dashboard_AWTBA.AutoSize = true;
+            this.dashboard_AWTBA.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dashboard_AWTBA.Location = new System.Drawing.Point(103, 31);
+            this.dashboard_AWTBA.Name = "dashboard_AWTBA";
+            this.dashboard_AWTBA.Size = new System.Drawing.Size(36, 37);
+            this.dashboard_AWTBA.TabIndex = 5;
+            this.dashboard_AWTBA.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(5, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(232, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Animals waiting to be adopted";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(138)))), ((int)(((byte)(36)))));
+            this.panel4.Controls.Add(this.dashboard_AA);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(284, 18);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(242, 110);
+            this.panel4.TabIndex = 1;
+            // 
+            // dashboard_AA
+            // 
+            this.dashboard_AA.AutoSize = true;
+            this.dashboard_AA.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dashboard_AA.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dashboard_AA.Location = new System.Drawing.Point(98, 31);
+            this.dashboard_AA.Name = "dashboard_AA";
+            this.dashboard_AA.Size = new System.Drawing.Size(36, 37);
+            this.dashboard_AA.TabIndex = 3;
+            this.dashboard_AA.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(60, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Active Adoption";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.panel3.Controls.Add(this.dashboard_TA);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(18, 18);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(242, 110);
+            this.panel3.TabIndex = 0;
+            // 
+            // dashboard_TA
+            // 
+            this.dashboard_TA.AutoSize = true;
+            this.dashboard_TA.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dashboard_TA.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dashboard_TA.Location = new System.Drawing.Point(104, 31);
+            this.dashboard_TA.Name = "dashboard_TA";
+            this.dashboard_TA.Size = new System.Drawing.Size(36, 37);
+            this.dashboard_TA.TabIndex = 1;
+            this.dashboard_TA.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(67, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total Animal";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Dashboard";
+            this.Size = new System.Drawing.Size(852, 526);
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label dashboard_AWTBA;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label dashboard_AA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label dashboard_TA;
+        private System.Windows.Forms.Label label1;
+    }
+}
