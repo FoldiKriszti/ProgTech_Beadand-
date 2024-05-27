73598f81e9658d43a77f6e9674998153b1e0b5f3
FoldiKriszti <fkrt2001@gmail.com>
2024-05-27 17:59:20 +0200

    Add Pet menüpont kidolgozása

---
+++ Program/ShelterManagementSys/ShelterManagementSys/User Control Panels/AddAnimal.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace ShelterManagementSys.User_Control_Panels
+{
+    partial class AddAnimal
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_DeleteAnimal = new System.Windows.Forms.Button();
+            this.btn_UpdateAnimal = new System.Windows.Forms.Button();
+            this.btn_AddAnimal = new System.Windows.Forms.Button();
+            this.addAdoption = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addSpecies = new System.Windows.Forms.ComboBox();
+            this.addAge = new System.Windows.Forms.TextBox();
+            this.addName = new System.Windows.Forms.TextBox();
+            this.addAnimalID = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AddEntry = new System.Windows.Forms.DateTimePicker();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.AddEntry);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btn_DeleteAnimal);
+            this.panel2.Controls.Add(this.btn_UpdateAnimal);
+            this.panel2.Controls.Add(this.btn_AddAnimal);
+            this.panel2.Controls.Add(this.addAdoption);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.addSpecies);
+            this.panel2.Controls.Add(this.addAge);
+            this.panel2.Controls.Add(this.addName);
+            this.panel2.Controls.Add(this.addAnimalID);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.Location = new System.Drawing.Point(16, 298);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(817, 209);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(138)))), ((int)(((byte)(36)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(508, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 44);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btn_DeleteAnimal
+            // 
+            this.btn_DeleteAnimal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(138)))), ((int)(((byte)(36)))));
+            this.btn_DeleteAnimal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_DeleteAnimal.FlatAppearance.BorderSize = 0;
+            this.btn_DeleteAnimal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.btn_DeleteAnimal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.btn_DeleteAnimal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DeleteAnimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_DeleteAnimal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_DeleteAnimal.Location = new System.Drawing.Point(399, 142);
+            this.btn_DeleteAnimal.Name = "btn_DeleteAnimal";
+            this.btn_DeleteAnimal.Size = new System.Drawing.Size(103, 44);
+            this.btn_DeleteAnimal.TabIndex = 15;
+            this.btn_DeleteAnimal.Text = "DELETE";
+            this.btn_DeleteAnimal.UseVisualStyleBackColor = false;
+            // 
+            // btn_UpdateAnimal
+            // 
+            this.btn_UpdateAnimal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(138)))), ((int)(((byte)(36)))));
+            this.btn_UpdateAnimal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_UpdateAnimal.FlatAppearance.BorderSize = 0;
+            this.btn_UpdateAnimal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.btn_UpdateAnimal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.btn_UpdateAnimal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_UpdateAnimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_UpdateAnimal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_UpdateAnimal.Location = new System.Drawing.Point(290, 142);
+            this.btn_UpdateAnimal.Name = "btn_UpdateAnimal";
+            this.btn_UpdateAnimal.Size = new System.Drawing.Size(103, 44);
+            this.btn_UpdateAnimal.TabIndex = 14;
+            this.btn_UpdateAnimal.Text = "UPDATE";
+            this.btn_UpdateAnimal.UseVisualStyleBackColor = false;
+            // 
+            // btn_AddAnimal
+            // 
+            this.btn_AddAnimal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(138)))), ((int)(((byte)(36)))));
+            this.btn_AddAnimal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_AddAnimal.FlatAppearance.BorderSize = 0;
+            this.btn_AddAnimal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.btn_AddAnimal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(52)))), ((int)(((byte)(39)))));
+            this.btn_AddAnimal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AddAnimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_AddAnimal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_AddAnimal.Location = new System.Drawing.Point(181, 142);
+            this.btn_AddAnimal.Name = "btn_AddAnimal";
+            this.btn_AddAnimal.Size = new System.Drawing.Size(103, 44);
+            this.btn_AddAnimal.TabIndex = 13;
+            this.btn_AddAnimal.Text = "ADD";
+            this.btn_AddAnimal.UseVisualStyleBackColor = false;
+            this.btn_AddAnimal.Click += new System.EventHandler(this.btn_AddAnimal_Click);
+            // 
+            // addAdoption
+            // 
+            this.addAdoption.FormattingEnabled = true;
+            this.addAdoption.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.addAdoption.Location = new System.Drawing.Point(549, 20);
+            this.addAdoption.Name = "addAdoption";
+            this.addAdoption.Size = new System.Drawing.Size(121, 21);
+            this.addAdoption.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(23, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Name";
+            // 
+            // addSpecies
+            // 
+            this.addSpecies.FormattingEnabled = true;
+            this.addSpecies.Items.AddRange(new object[] {
+            "Dog",
+            "Cat"});
+            this.addSpecies.Location = new System.Drawing.Point(147, 68);
+            this.addSpecies.Name = "addSpecies";
+            this.addSpecies.Size = new System.Drawing.Size(180, 21);
+            this.addSpecies.TabIndex = 10;
+            // 
+            // addAge
+            // 
+            this.addAge.Location = new System.Drawing.Point(147, 95);
+            this.addAge.Name = "addAge";
+            this.addAge.Size = new System.Drawing.Size(180, 20);
+            this.addAge.TabIndex = 9;
+            // 
+            // addName
+            // 
+            this.addName.Location = new System.Drawing.Point(147, 42);
+            this.addName.Name = "addName";
+            this.addName.Size = new System.Drawing.Size(180, 20);
+            this.addName.TabIndex = 6;
+            // 
+            // addAnimalID
+            // 
+            this.addAnimalID.Location = new System.Drawing.Point(147, 16);
+            this.addAnimalID.Name = "addAnimalID";
+            this.addAnimalID.Size = new System.Drawing.Size(180, 20);
+            this.addAnimalID.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(23, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 16);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Age:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(355, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Is someone adopting him/her?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(23, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Species of Animal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(23, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Animal ID:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(16, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(817, 278);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(138)))), ((int)(((byte)(36)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(43)))), ((int)(((byte)(36)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 214);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Animal\'s Data";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(470, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Entry Date:";
+            // 
+            // AddEntry
+            // 
+            this.AddEntry.Location = new System.Drawing.Point(549, 47);
+            this.AddEntry.Name = "AddEntry";
+            this.AddEntry.Size = new System.Drawing.Size(121, 20);
+            this.AddEntry.TabIndex = 18;
+            // 
+            // AddAnimal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "AddAnimal";
+            this.Size = new System.Drawing.Size(852, 526);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_DeleteAnimal;
+        private System.Windows.Forms.Button btn_UpdateAnimal;
+        private System.Windows.Forms.Button btn_AddAnimal;
+        private System.Windows.Forms.ComboBox addAdoption;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox addSpecies;
+        private System.Windows.Forms.TextBox addAge;
+        private System.Windows.Forms.TextBox addName;
+        private System.Windows.Forms.TextBox addAnimalID;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker AddEntry;
+        private System.Windows.Forms.Label label6;
+    }
+}
