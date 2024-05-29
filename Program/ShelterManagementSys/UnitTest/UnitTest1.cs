1a0bcee0738911328fc713c9e376cabf925910db
Dalma <dalmicsku72@gmail.com>
2024-05-29 18:35:33 +0200

    Unit test létrehozása

---
+++ Program/ShelterManagementSys/UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShelterManagementSys.User_Control_Panels;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddAnimal_AddAnimalToDatabase()
+        {
+            var shelter = new AddAnimal();
+
+            
+        }
+    }
+}
